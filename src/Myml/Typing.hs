9da7ab498c8f6ecdf45dbb0122fc3505e12b157c
Lin Yinfeng <lin.yinfeng@outlook.com>
2020-02-18 22:24:03 +0800

    Add signature for function unify

--- src/Myml/Typing.hs
+++ src/Myml/Typing.hs
@@ -1,3 +1,15 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Myml.Typing
-  ()
+  ( unify
+  )
 where
+
+import           Myml.Subst
+import           Data.Equivalence.Monad
+
+unify
+  :: TypeSubstitutor
+  -> TypeSubstitutor
+  -> (forall s . EquivT s TypeSubstitutor TypeSubstitutor m ())
+unify = error ""
